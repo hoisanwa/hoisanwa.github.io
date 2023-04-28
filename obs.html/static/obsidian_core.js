5293fc8f9fbbacc8cb77ac338de2d3c5246af1f3
Ziwei Huang <huang-ziwei@outlook.com>
2023-04-29 01:11:10 +0200

    update

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '268295447782160354577369051938219695087'                       // used to check whether the localStorage data is stale
+var gzip_hash = '176982493379562472360064094147627624171'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
